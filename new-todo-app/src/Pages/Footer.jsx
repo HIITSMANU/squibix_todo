d76d5e92e2f66ee6d22479ebf884f6ccb232691c
HIITSMANU <132491372+HIITSMANU@users.noreply.github.com>
2024-07-21 10:58:00 +0530

    [todo_project_done]

--- new-todo-app/src/Pages/Footer.jsx
+++ new-todo-app/src/Pages/Footer.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useTheme } from '../Contexts/ThemeContext'
+import "./Footer.css"
 
 const Footer = () => {
 
@@ -7,7 +8,7 @@ const Footer = () => {
   return (
     <>
         <footer className={`w-full ${theme === 'light' ? 'bg-white':'bg-gray-800 text-white'}  text-center`}>
-            <span>Made with <i class="fa fa-heart pulse text-red-700"></i> by S.Manohar</span>
+            <span>Made with <i class="fa fa-heart pulse text-red-700 text-[10px]"></i> by S.Manohar</span>
         </footer>
     </>
   )
